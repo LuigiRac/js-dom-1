4349bb48a3dd6fbdfbbb87643b4a945cab4458ce
LuigiRac <lui.rac06@gmail.com>
2024-11-12 13:31:40 +0100

    end exercise

--- script/script.js
+++ script/script.js
@@ -1,5 +1,19 @@
 "use strict"
 console.clear()
 
-let btn = document.getElementById("on");
-let img = document.getElementById("image");
+let btn = document.getElementById("btn")
+let img = document.getElementById("image")
+
+btn.addEventListener("click", changeBtn);
+
+function changeBtn() {
+    if (btn.textContent.includes("accendi")) {
+        img.src = "img/yellow_lamp.png"
+        btn.textContent = "spegni"
+    }
+    else {
+        img.src = "img/white_lamp.png"
+        btn.textContent = "accendi"
+    }
+}
+

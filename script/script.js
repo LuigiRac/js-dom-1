940450083ff2a015175e949d647f0516185134c3
LuigiRac <lui.rac06@gmail.com>
2024-11-12 01:23:52 +0100

    add const light

--- script/script.js
+++ script/script.js
@@ -1,3 +1,8 @@
 "use strict"
 console.clear()
 
+const lightOff = document.getElementById("off");
+const lightOn = document.getElementById("on");
+const image = document.getElementById("image");
+
+off.addeventListner("clik")
